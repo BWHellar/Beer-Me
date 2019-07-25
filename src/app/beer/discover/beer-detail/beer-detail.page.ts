01c291e6a96408de1cb97b55f948bd43ec1fdc00
bh <bwhellar@gmail.com>
2019-07-25 10:52:21 -0700

    put in a back button for the beers

--- src/app/beer/discover/beer-detail/beer-detail.page.ts
+++ src/app/beer/discover/beer-detail/beer-detail.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-beer-detail',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeerDetailPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router, private navCtrl: NavController) { }
 
   ngOnInit() {
+  }
+
+  onSaveBeer() {
+    this.navCtrl.navigateBack('/beer/tabs/discover');
   }
 
 }
