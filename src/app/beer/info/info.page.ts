d3ba74a6c26d17d320f0e73a5ca9ea8b01cdaea2
bh <bwhellar@gmail.com>
2019-07-25 12:09:01 -0700

    allow for personal tapped beers to display in a list

--- src/app/beer/info/info.page.ts
+++ src/app/beer/info/info.page.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BeerService } from '../beer.service';
+import { Beer } from '../beer.model';
+
 @Component({
   selector: 'app-info',
   templateUrl: './info.page.html',
   styleUrls: ['./info.page.scss'],
 })
 export class InfoPage implements OnInit {
+  tryBeers: Beer[];
 
-  constructor() { }
+  constructor(private beerService: BeerService) { }
 
   ngOnInit() {
+    this.tryBeers = this.beerService.beer;
   }
 
 }
