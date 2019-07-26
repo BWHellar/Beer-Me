9b335563b173df3c289a6cc689b2e3d93e757a6c
Brendan Hellar <bwhellar@gmail.com>
2019-07-26 16:47:35 -0700

    put in some forms for adding a new beer to personal list

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -13,6 +13,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(private authService: AuthService, private router: Router, private loadingCtrl: LoadingController) { }
 
@@ -34,7 +35,22 @@ export class AuthPage implements OnInit {
     });
   }
 
+  onSwitchAuthMode() {
+    this.isLogin =!this.isLogin;
+  }
+
   onSubmit(form: NgForm){
-    console.log(form);
+    if(!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+    console.log(email, password);
+
+    if(this.isLogin) {
+      //Server Stuff
+    } else {
+      //Server Stuff
+    }
   }
 }
