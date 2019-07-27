02bd49c4312161cd2cebad1326a0a9bae173d9a3
Brendan Hellar <bwhellar@gmail.com>
2019-07-26 17:10:05 -0700

    test to see if input form works

--- src/app/beer/info/new-beer/new-beer.page.ts
+++ src/app/beer/info/new-beer/new-beer.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-new-beer',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-beer.page.scss'],
 })
 export class NewBeerPage implements OnInit {
+  form: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      description: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      }),
+      price: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required, Validators.min(1)]
+      }),
+      date: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
+    });
+  }
+
+  onCreateBeer() {
+    console.log(this.form);
   }
 
 }
